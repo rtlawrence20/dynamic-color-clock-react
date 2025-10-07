6f4e9f829aec0d898e3fa34c606aa63d2a0642de
Robert Lawrence <29217166+rtlawrence20@users.noreply.github.com>
2025-10-07 11:02:43 -0600

    updated documentation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,23 @@ import { useState, useEffect } from 'react'
 import { format } from 'date-fns'
 import './App.css'
 
+/**
+ * App displays a real-time clock.
+ *
+ * useState and useEffect hooks from React are used to track and update the current time
+ * format is used from 'date-fns` to gracefully display time and date.
+ *
+ * @component
+ * @returns {JSX.Element} A container with the current formatted date and time
+ *
+ * @description
+ * - `useState` initializes and stores the current Date object.
+ * - `useEffect` starts a timer that updates the `now` state every second.
+ * - The timer is cleared when component unmounts to prevent memory leaks.
+ * - Formatting of date/time is as follows: 
+ * - - "MMM do, yyyy" (e.g., Oct 7th, 2025)
+ * - - "hh:mm:ss a" (e.g., 05:23:10 PM).
+ */
 function App() {
     // need current time and a way to set a 'future' current time
     const [now, setNow] = useState(new Date());
